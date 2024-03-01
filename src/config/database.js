4be01fbecc0ddf60a7b7622f6e4eb65e8cdb2b50
Nastsukii <lacb.lzn.luiz@gmail.com>
2024-03-01 11:00:04 -0300

    criando users

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'postgres',
-  database: 'codeburguer',
+  database: 'codebuguer',
   define: {
     timestamps: true,
     underscored: true,
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'hello world' })
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Luiz',
+    email: 'luiz@email.com',
+    password_hash: 'kdsfu9',
+  })
+  return response.json(user)
 })
 
 export default routes
